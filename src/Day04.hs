0f834a69bbeb34ab4c414c8d669f688a7533bea2
Bjørnar Snoksrud <bsnoksru@cisco.com>
2018-12-14 21:19:11 +0100

    day04, simplify

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,13 +1,13 @@
 module Day04 where
 
 import Data.Char (ord)
-import Data.List (find)
+import Data.List (findIndex)
 import Data.Maybe (fromJust)
 
 import Data.Word (Word8)
 import Crypto.Hash.MD5 (hash)
 import Data.ByteString (pack, unpack)
-import Text.Printf
+import Text.Printf (printf)
 
 toHex :: Word8 -> String
 toHex w = let x = ['0'..'9'] <> "abcdef"
@@ -18,14 +18,14 @@ hash' :: String -> String
 hash' = concatMap toHex . unpack . hash . pack . (fromIntegral . ord <$>)
 
 mine :: String -> Int -> String
-mine seed i = hash' $ seed <> show i
+mine seed = hash' . (seed <>) .  show
 
-zeroes :: Int -> [(Int, String)] -> Int
-zeroes n = fst . fromJust . find ((== replicate n '0') .  take n . snd)
+zeroes :: Int -> [String] -> Int
+zeroes n = fromJust . findIndex ((== replicate n '0') .  take n)
 
 day04 :: IO ()
 day04 = do
   let key = "iwrupvqb"
-  let candidates = map  (\x -> (x, mine key x)) [0..]
+  let candidates = mine key <$> [0..]
   printf "Part1: %d\n" $ zeroes 5 candidates
   printf "Part2: %d\n" $ zeroes 6 candidates
